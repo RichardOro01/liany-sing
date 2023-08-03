dbd2f8050a168fe8f3d2f36d19cdc90c0abe7f4a
Richard.Oropesa <93955101+RichardOro01@users.noreply.github.com>
2023-08-03 19:22:10 -0400

    feat: action dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 
+interface Option {
+  text: string;
+  action: () => void;
+}
+
 interface DialogProps {
   texts: string[];
   setDialoging: React.Dispatch<React.SetStateAction<boolean>>;
+  options: Option[];
 }
 
-const Dialog: React.FC<DialogProps> = ({ texts, setDialoging }) => {
+const Dialog: React.FC<DialogProps> = ({ texts, setDialoging, options }) => {
   const [currentText, setCurrentText] = useState(0);
   const [showingText, setShowingText] = useState("");
   const [timer, setTimer] = useState<NodeJS.Timer>();
@@ -63,10 +69,20 @@ const Dialog: React.FC<DialogProps> = ({ texts, setDialoging }) => {
 
   return (
     <div
-      className="fixed bottom-5 mx-5 p-4 border-2 shadow-lg rounded-lg bg-slate-50 w-11/12 min-h-[60px] z-40"
+      className="flex flex-col gap-2 fixed bottom-5 mx-5 p-4 border-2 shadow-lg rounded-lg bg-slate-50 w-11/12 min-h-[60px] z-40"
       onClick={skipTimer}
     >
-      {showingText}
+      <div>{showingText}</div>
+      {currentText === texts.length - 1 &&
+        showingText.length === texts[currentText].length && (
+          <div className="flex flex-row gap-4 w-full justify-end text-slate-500">
+            {options.map((option, index) => (
+              <span key={`${index}-${option}`} onClick={option.action}>
+                {option.text}
+              </span>
+            ))}
+          </div>
+        )}
     </div>
   );
 };
--- src/components/scenes/Hello.tsx
+++ src/components/scenes/Hello.tsx
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 import Dialog from "../Dialog";
 import Richard from "../Richard";
+import { useDispatch } from "react-redux";
+import { setScene, setTransition } from "@/core/stores/scene";
 
 const Hello = () => {
+  const dispatch = useDispatch();
   const [dialoging, setDialoging] = useState(false);
+  const handleNext = () => {
+    dispatch(setTransition(true));
+
+    setTimeout(() => {
+      dispatch(setScene("singing"));
+      dispatch(setTransition(false));
+    }, 1000);
+  };
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <Richard state={dialoging ? "talking" : "smiling"} />
@@ -15,6 +26,7 @@ const Hello = () => {
           "Vamos!",
         ]}
         {...{ setDialoging }}
+        options={[{ text: "Vamos!", action: handleNext }]}
       />
     </div>
   );
