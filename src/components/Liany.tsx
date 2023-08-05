c9401aa7ef054b1ae00d3fe749ab1a798a78d0e1
Richard.Oropesa <93955101+RichardOro01@users.noreply.github.com>
2023-08-04 23:25:32 -0400

    faet: sing animation

--- src/components/Liany.tsx
+++ src/components/Liany.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image, { StaticImageData } from "next/image";
 import idle from "../assets/sprites/Liany/idle.png";
 import arm from "../assets/sprites/Liany/arm.png";
@@ -10,19 +10,159 @@ import eyes from "../assets/sprites/eyes/open.png";
 import guitar from "../assets/guitar.webp";
 import closedMouth from "../assets/sprites/mouth/closed mouth.png";
 import openMouth from "../assets/sprites/mouth/open mouth.png";
+import semiopenMouth from "../assets/sprites/mouth/semiopen mouth.png";
 import sadMouth from "../assets/sprites/mouth/sad mouth.png";
 import smilingMouth from "../assets/sprites/mouth/smiling mouth.png";
 import styles from "../styles/liany.module.css";
 
-interface RichardProps {
-  state: "talking" | "smiling" | "sad" | "serious";
+interface LianyProps {
+  state: LianyState;
   className: string;
+  setState: React.Dispatch<React.SetStateAction<LianyState>>;
 }
 
-const Liany: React.FC<RichardProps> = ({ state, className }) => {
+export type LianyState = "talking" | "smiling" | "sad" | "serious" | "singing";
+
+const Liany: React.FC<LianyProps> = ({ state, className, setState }) => {
+  const mouthTimes = [
+    { mouth: closedMouth, time: 1500 },
+    { mouth: openMouth, time: 500 }, //I
+    { mouth: semiopenMouth, time: 600 }, //still re
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 500 }, //me
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 500 }, //ber
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 500 }, //the third
+    { mouth: openMouth, time: 500 }, //of
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 200 }, //de
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 200 }, //cemb
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 500 }, //ber
+
+    { mouth: closedMouth, time: 500 },
+    { mouth: semiopenMouth, time: 500 }, // me in
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 500 }, //your
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 500 }, //swea
+    { mouth: semiopenMouth, time: 500 }, //ter
+    { mouth: closedMouth, time: 500 },
+    { mouth: semiopenMouth, time: 500 }, //you
+    { mouth: openMouth, time: 200 }, //said
+    { mouth: semiopenMouth, time: 100 }, //it looked
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 400 }, //it looked
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 200 }, //be
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 500 }, //tter
+
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 100 }, //on
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 500 }, //me
+    { mouth: openMouth, time: 200 }, //than
+    { mouth: semiopenMouth, time: 200 }, //it
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 200 }, //did
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 500 }, //you
+    { mouth: closedMouth, time: 700 },
+    { mouth: openMouth, time: 500 }, //On
+    { mouth: semiopenMouth, time: 500 }, //ly
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 200 }, //if
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 200 }, //you
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 500 }, //knew
+
+    { mouth: closedMouth, time: 500 },
+    { mouth: openMouth, time: 200 }, //how
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 300 }, //much
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 100 }, //I
+    { mouth: semiopenMouth, time: 500 },
+    { mouth: openMouth, time: 300 }, //li
+    { mouth: semiopenMouth, time: 400 }, //ed you
+    { mouth: closedMouth, time: 300 },
+    { mouth: semiopenMouth, time: 200 }, //But,
+    { mouth: openMouth, time: 400 }, //I
+    { mouth: semiopenMouth, time: 100 },
+    { mouth: openMouth, time: 200 }, //Watch,
+    { mouth: semiopenMouth, time: 200 }, //your
+    { mouth: openMouth, time: 500 }, //eyes,
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 200 }, //as,
+    { mouth: semiopenMouth, time: 500 }, //she,
+
+    { mouth: openMouth, time: 1000 }, //Walks
+    { mouth: closedMouth, time: 100 },
+    { mouth: openMouth, time: 500 }, //By
+    { mouth: closedMouth, time: 900 },
+    { mouth: openMouth, time: 500 }, //What
+    { mouth: semiopenMouth, time: 100 },
+    { mouth: openMouth, time: 500 }, //a
+    { mouth: semiopenMouth, time: 100 },
+    { mouth: openMouth, time: 500 }, //sight
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 500 }, //for
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 1200 }, //sor
+    { mouth: openMouth, time: 800 }, //eyes
+
+    { mouth: closedMouth, time: 1000 },
+    { mouth: openMouth, time: 500 }, //Bright
+    { mouth: semiopenMouth, time: 700 }, //er
+    { mouth: openMouth, time: 500 }, //than
+    { mouth: semiopenMouth, time: 500 }, //the
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 1000 }, //blue
+    { mouth: openMouth, time: 700 }, //sky
+
+    { mouth: closedMouth, time: 700 },
+    { mouth: semiopenMouth, time: 700 }, //She`s
+    { mouth: openMouth, time: 500 }, //got
+    { mouth: semiopenMouth, time: 500 }, //you
+    { mouth: closedMouth, time: 300 },
+    { mouth: semiopenMouth, time: 300 }, //mes
+    { mouth: closedMouth, time: 100 },
+    { mouth: semiopenMouth, time: 1200 }, //meri
+    { mouth: openMouth, time: 700 }, //zed
+
+    { mouth: closedMouth, time: 700 },
+    { mouth: semiopenMouth, time: 300 }, //While
+    { mouth: openMouth, time: 1200 }, //I
+    { mouth: closedMouth, time: 200 },
+    { mouth: openMouth, time: 500 }, //die
+    { mouth: closedMouth, time: 100 }, //die
+  ];
+
   const [currentMouth, setCurrentMouth] =
     useState<StaticImageData>(smilingMouth);
   const [timer, setTimer] = useState<NodeJS.Timer>();
+  const audio = useRef<HTMLAudioElement | undefined>(
+    typeof Audio !== "undefined" ? new Audio("/audio/singing.mp3") : undefined
+  );
+
+  const playAudio = () => {
+    audio.current?.play();
+    audio.current?.addEventListener("ended", () => {
+      setState("serious");
+    });
+    singMouth(0);
+  };
+
+  const singMouth = (current: number) => {
+    setCurrentMouth(mouthTimes[current].mouth);
+    if (current < mouthTimes.length - 1 && mouthTimes[current].time) {
+      setTimeout(() => singMouth(current + 1), mouthTimes[current].time);
+    }
+  };
 
   useEffect(() => {
     killTimer();
@@ -38,6 +178,9 @@ const Liany: React.FC<RichardProps> = ({ state, className }) => {
         break;
       case "talking":
         setTimer(setTimeout(talk, 100));
+        break;
+      case "singing":
+        playAudio();
         break;
       default:
         setCurrentMouth(smilingMouth);
@@ -69,12 +212,14 @@ const Liany: React.FC<RichardProps> = ({ state, className }) => {
   };
 
   return (
-    <div className={`relative w-80 ${className}`}>
+    <div className={`relative w-80 ${className}`} onClick={playAudio}>
       <Image src={idle} alt="Liany" className="absolute" draggable={false} />
       <Image
         src={arm}
         alt="arm"
-        className={`absolute top-64 left-[182px] w-20 ${styles.left_arm_playing}`}
+        className={`absolute top-64 left-[182px] w-20 ${
+          state === "singing" ? styles.left_arm_playing : ""
+        }`}
         draggable={false}
       />
       <Image
@@ -104,20 +249,30 @@ const Liany: React.FC<RichardProps> = ({ state, className }) => {
       <Image
         src={rightForearm}
         alt="right forearm"
-        className={`absolute top-[350px] left-[75px] w-[113px] ${styles.right_forearm_playing}`}
+        className={`absolute top-[350px] left-[75px] w-[113px] ${
+          state === "singing"
+            ? styles.right_forearm_playing
+            : styles.right_forearm
+        }`}
         draggable={false}
       />
       <Image
         src={leftForearm}
         alt="left forearm"
-        className={`absolute top-[268px] left-[208px] w-[39px] ${styles.left_forearm_playing}`}
+        className={`absolute top-[268px] left-[208px] w-[39px] ${
+          state === "singing"
+            ? styles.left_forearm_playing
+            : styles.left_forearm
+        }`}
         draggable={false}
       />
 
       <Image
         src={currentMouth}
         alt="mouth"
-        className={`absolute top-60 left-32 w-16`}
+        className={`absolute top-60 left-32 w-16 ${
+          state === "singing" ? styles.mouth_singing : ""
+        }`}
         draggable={false}
       />
     </div>
--- src/components/scenes/Singing.tsx
+++ src/components/scenes/Singing.tsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "@/styles/stage.module.css";
-import Richard from "../Richard";
 import micro from "@/assets/micro.png";
-import Liany from "../Liany";
+import Liany, { LianyState } from "../Liany";
+import { Button } from "antd";
+import JSConfetti from "js-confetti";
 
 const Singing = () => {
+  const [state, setState] = useState<LianyState>("smiling");
+  useEffect(() => {
+    const jsConfetti = new JSConfetti();
+
+    jsConfetti.addConfetti({ confettiNumber: 1000 });
+  }, []);
+
   return (
     <div className={`relative w-full h-screen ${styles.stage}`}>
-      {/* <Image
-        src={"/stage.webp"}
-        alt="stage"
-        width={920}
-        height={1080}
-        className="absolute min-w-[2920px] min-h-[1080px]"
-      /> */}
       <Liany
-        state="smiling"
+        {...{ state, setState }}
         className="top-[60px] left-1/2 -translate-x-1/2 absolute"
       />
       <Image
@@ -24,6 +25,13 @@ const Singing = () => {
         alt="micro"
         className="absolute w-28 top-[330px] left-[40%] -translate-x-1/2"
       />
+      <Button
+        size="large"
+        className=" top-[60px] left-1/2 -translate-x-1/2 absolute "
+        onClick={() => setState("singing")}
+      >
+        Sing
+      </Button>
     </div>
   );
 };
